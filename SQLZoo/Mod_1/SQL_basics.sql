a98ee0ce02e6cdf28ab1fb20c2e376a7ad32be87
Tony Dolete <kheops97@outlook.com>
2020-04-23 12:27:05 +0300

    Create SQL_basics.sql

---
+++ SQLZoo/Mod_1/SQL_basics.sql
@@ -0,0 +1,9 @@
+Problema 1.
+/* 
+The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be 
+in 'single quotes';
+Modify it to show the population of Germany
+*/
+
+SELECT name, population FROM world
+WHERE name = 'Germany';
